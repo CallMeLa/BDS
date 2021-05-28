f4642b0170df676ecdd508be8d0af6f0bbf8aab2
LA M <84457316+CallMeLa@users.noreply.github.com>
2021-05-28 16:24:04 +0100

    Create IPBlocker.cmd

---
+++ IPBlocker.cmd
@@ -0,0 +1,11 @@
+Usage example: Enter an IP address to test it.
+Requirements: $alwaysReply type of callback. (a-Z triggers will work also).
+Variables: No variables needed.
+Command language: BDScript
+
+Command trigger: $alwaysReply
+Code:
+
+$if[$checkContains[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$noMentionMessage;9;#;-1];8;#;-1];7;#;-1];6;#;-1];5;#;-1];4;#;-1];3;#;-1];2;#;-1];1;#;-1];0;#;-1];###.###.##.###;###.###.###.###;###.###.###.##;###.###.###.#;##.###.###.###;##.###.###.##;##.###.###.#;###.###.##.##;###.###.#.#;##.###.#.#;##.##.##.###;###.###.#.###;###.###.#;###.###.#.#;###.##.###.###;###.##.#.#;###.#.#.#;##.#.###.###;##.###.##.#;###.##.#.##;##.###.##.###;###.###.##.#;###.##.##.#;#.###.##.###;#.###.#.###;###.##.###.##;#.###.###.###;##.##.###.##]]
+$deletecommand
+$endif
