f83640f0495b337314599f78a538595bbea598ff
LA M <84457316+CallMeLa@users.noreply.github.com>
2021-09-20 17:10:42 +0100

    Create badwords.cmd

---
+++ badwords.cmd
@@ -0,0 +1,4 @@
+$c[Add this code to your $alwaysReply or a-Z command triggers. BDScript 2.]
+$nomention $suppressErrors $httpGet[https://callmela.github.io/badwords-json/badwords.json]
+$if[$checkContains[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$toLowercase[$message];?;;-1];.;;-1];,;;-1];-;;-1]; ;;-1];$httpResult]==true]
+<@$authorID> please avoid using those words. $deletecommand $endif
